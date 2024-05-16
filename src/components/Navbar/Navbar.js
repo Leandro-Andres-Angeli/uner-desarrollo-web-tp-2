132ce85f7a65e03aa54902b12034a83bf3d83fcd
Debora <deborainfinit69@gmail.com>
2024-05-16 17:58:22 -0300

    cambie sintaxis en navbarbtn

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,7 +2,7 @@ import React, { useLayoutEffect, useRef, useState } from 'react';
 import { SiteIcon } from '../../icons/SiteIcon';
 import { linksData } from '../../data/routes';
 import { Link, NavLink } from 'react-router-dom/cjs/react-router-dom.min';
-import NavbarBtn from './NavbarBtn';
+import NavbarBtn from './NavbarBtn.js';
 import navbarStyles from './navbar.module.css';
 const Navbar = () => {
   const { nav, navLogo, navLinks, header, hidden, visible } = navbarStyles;
