2f8cd561fe7408e434a22fb7410eba82aa30f3a4
Debora <deborainfinit69@gmail.com>
2024-05-17 22:08:45 -0300

    actualizo formulario contacto

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -30,53 +30,55 @@ const Formulario = () => {
   };
 
   return (
-    <div className="contacto">
+    <main>
+      <div className="contacto">
       <h1>Contacto</h1>
       <h2>Ingresá los datos para tu reserva</h2>
-      <form className="formulario" onSubmit={handleSubmit}>
-        <label htmlFor="nombre" >Nombre:</label>
-        <input 
-          type="text"
-          id="nombre"
-          name="nombre"
-          placeholder="Ingresa tu nombre"
-          value={datosFormulario.nombre}
-          onChange={handleChange}
-          required
-        />
-        <label htmlFor="email">E-mail:</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          placeholder="ej.correo@ejemplo.com"
-          value={datosFormulario.email}
-          onChange={handleChange}
-          required
-        />
-        <label htmlFor="telefono">Teléfono:</label>
-        <input
-          type="tel"
-          id="telefono"
-          name="telefono"
-          placeholder="Ingresa tu teléfono"
-          value={datosFormulario.telefono}
-          onChange={handleChange}
-          required
-        />
-        <label htmlFor="mensaje">Mensaje:</label>
-        <textarea
-          id="mensaje"
-          name="mensaje"
-          className="mensajetextarea"
-          placeholder="Escribe tu mensaje aquí"
-          value={datosFormulario.mensaje}
-          onChange={handleChange}
-          required
-        ></textarea>
-        <button className="icon-btn btn-index ml-30" type="submit">ENVIAR</button>
-      </form>
-    </div>
+      </div>
+        <form className="formulario" onSubmit={handleSubmit}>
+          <label htmlFor="nombre" >Nombre:</label>
+            <input 
+            type="text"
+            id="nombre"
+            name="nombre"
+            placeholder="Ingresa tu nombre"
+            value={datosFormulario.nombre}
+            onChange={handleChange}
+            required
+            />
+          <label htmlFor="email">E-mail:</label>
+            <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="ej.correo@ejemplo.com"
+            value={datosFormulario.email}
+           onChange={handleChange}
+            required
+            />
+          <label htmlFor="telefono">Teléfono:</label>
+            <input
+            type="tel"
+            id="telefono"
+            name="telefono"
+            placeholder="Ingresa tu teléfono"
+            value={datosFormulario.telefono}
+            onChange={handleChange}
+            required
+            />
+          <label htmlFor="mensaje">Mensaje:</label>
+            <textarea
+            id="mensaje"
+            name="mensaje"
+            className="mensajetextarea"
+            placeholder="Escribe tu mensaje aquí"
+            value={datosFormulario.mensaje}
+            onChange={handleChange}
+            required
+            ></textarea>
+          <button className="icon-btn btn-index ml-30" type="submit">ENVIAR</button>
+        </form>
+    </main>
   );
 }
 
