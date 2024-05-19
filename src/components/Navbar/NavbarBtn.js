704a28ed716b24d7473560dc85d909fc8abf09f1
= <=>
2024-05-19 19:12:00 -0300

    btn navbar done

--- src/components/Navbar/NavbarBtn.js
+++ src/components/Navbar/NavbarBtn.js
@@ -1,6 +1,13 @@
 import React, { useEffect, useReducer, useRef, useState } from 'react';
 import navbarStyle from './navbarBtn.module.css';
-const NavbarBtn = ({ setMenuDisplay, hidden, visible, app }) => {
+const NavbarBtn = ({
+  setMenuDisplay,
+  hidden,
+  visible,
+  app,
+  mobileNavbarDisplay,
+  setmobileNavbarDisplay,
+}) => {
   const [iconRotate, setHandleRotate] = useState('');
   // const [isMobileMenuVisible, setIsMobileMenuVisible] = useState(false);
   const [mobileMenuVisible, setMobileMenuVisible] = useState('hidden');
@@ -9,62 +16,36 @@ const NavbarBtn = ({ setMenuDisplay, hidden, visible, app }) => {
   // };
   // const docWrapper =   document.body.querySelector('.App');
   const docWrapper = useRef();
-  const test = () => console.log('dont class');
-  // useEffect(() => {
-  //   docWrapper.current = document.body.querySelector('.App');
 
-  //   return () => {
-  //     console.log('onmount');
-  //     Boolean(docWrapper.current.classList.contains('navLinksMobileHidden')) ===
-  //     false
-  //       ? docWrapper.current.addEventListener('click', test)
-  //       : docWrapper.current.removeEventListener('click', test);
-  //   };
+  // useEffect(() => {
+  //   docWrapper.current = document.querySelector('.App');
   // }, []);
-  // const handleAppClassName = () => {
-  //   console.log('handling class');
-  //   docWrapper.current.classList.add('navLinksMobileHidden');
-  // };
+
   useEffect(() => {
-    console.log('render empy dep');
     docWrapper.current = document.querySelector('.App');
-    console.log(
-      docWrapper.current.setAttribute('data-mobile-nav-link', mobileMenuVisible)
-    );
-  }, [mobileMenuVisible]);
+    docWrapper.current.setAttribute('data-mobile-nav-link', mobileMenuVisible);
+    function handleOutSideClick(e) {
+      if (
+        e.target &&
+        e.target.closest('button') &&
+        e.target.closest('button').classList.contains('mobile-nav-btn')
+      ) {
+        return;
+      }
 
-  // useEffect(() => {
-  //   if (isMobileMenuVisible) {
-  //     docWrapper.current.addEventListener(
-  //       'click',
-  //       function (e) {
-  //         e.stopPropagation();
-
-  //         handleAppClassName();
-  //       },
-  //       true
-  //     );
-  //   }
-  //   return () => {
-  //     console.log('UNMOUNT');
-  //   };
-  // }, [isMobileMenuVisible]);
-
-  /* const handleMenuDisplay = (e) => {
-    console.log('menu display');
-    console.log(docWrapper.current);
-    docWrapper.current.classList.toggle('navLinksMobileHidden');
-    setMenuDisplay((prev) => (prev === hidden ? visible : hidden));
-  }; */
-
-  // const handleMenuDisplay = (e) => {
-  //   e.stopPropagation();
-  //   e.nativeEvent.stopImmediatePropagation();
-  //   setIsMobileMenuVisible((prev) => !prev);
-  //   isMobileMenuVisible
-  //     ? docWrapper.current.classList.remove('navLinksMobileHidden')
-  //     : docWrapper.current.classList.add('navLinksMobileHidden');
-  // };
+      setMobileMenuVisible('hidden');
+    }
+    if (mobileMenuVisible === 'visible') {
+      docWrapper.current.addEventListener('click', handleOutSideClick, true);
+    }
+    return () => {
+      return docWrapper.current.removeEventListener(
+        'click',
+        handleOutSideClick,
+        true
+      );
+    };
+  }, [docWrapper, mobileMenuVisible]);
 
   const handleMenuDisplay = () => {
     setMobileMenuVisible((prev) => (prev === 'hidden' ? 'visible' : 'hidden'));
@@ -72,7 +53,7 @@ const NavbarBtn = ({ setMenuDisplay, hidden, visible, app }) => {
   return (
     <button
       onPointerDown={handleMenuDisplay}
-      className={`${navbarStyle.navbarBtn} btn-primary ${iconRotate} ${navbarStyle.navbarBtnDisplay}`}
+      className={`${navbarStyle.navbarBtn} mobile-nav-btn btn-primary ${iconRotate} ${navbarStyle.navbarBtnDisplay}`}
     >
       <div>
         <span></span>
