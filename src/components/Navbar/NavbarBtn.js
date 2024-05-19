30fbe584c424b0614c1f46803fda9410caa5ee3b
= <=>
2024-05-19 15:41:53 -0300

    rearmando navbar mobile

--- src/components/Navbar/NavbarBtn.js
+++ src/components/Navbar/NavbarBtn.js
@@ -2,7 +2,8 @@ import React, { useEffect, useReducer, useRef, useState } from 'react';
 import navbarStyle from './navbarBtn.module.css';
 const NavbarBtn = ({ setMenuDisplay, hidden, visible, app }) => {
   const [iconRotate, setHandleRotate] = useState('');
-  const [isMobileMenuVisible, setIsMobileMenuVisible] = useState(false);
+  // const [isMobileMenuVisible, setIsMobileMenuVisible] = useState(false);
+  const [mobileMenuVisible, setMobileMenuVisible] = useState('hidden');
   // const handleRotate = () => {
   //   setHandleRotate((prev) => (prev === '' ? navbarStyle.rotate : ''));
   // };
@@ -20,31 +21,34 @@ const NavbarBtn = ({ setMenuDisplay, hidden, visible, app }) => {
   //       : docWrapper.current.removeEventListener('click', test);
   //   };
   // }, []);
-  const handleAppClassName = () => {
-    console.log('handling class');
-    docWrapper.current.classList.add('navLinksMobileHidden');
-  };
+  // const handleAppClassName = () => {
+  //   console.log('handling class');
+  //   docWrapper.current.classList.add('navLinksMobileHidden');
+  // };
   useEffect(() => {
     console.log('render empy dep');
     docWrapper.current = document.querySelector('.App');
-  }, []);
+    console.log(
+      docWrapper.current.setAttribute('data-mobile-nav-link', mobileMenuVisible)
+    );
+  }, [mobileMenuVisible]);
 
-  useEffect(() => {
-    if (isMobileMenuVisible) {
-      docWrapper.current.addEventListener(
-        'click',
-        function (e) {
-          e.stopPropagation();
+  // useEffect(() => {
+  //   if (isMobileMenuVisible) {
+  //     docWrapper.current.addEventListener(
+  //       'click',
+  //       function (e) {
+  //         e.stopPropagation();
 
-          handleAppClassName();
-        },
-        true
-      );
-    }
-    return () => {
-      console.log('UNMOUNT');
-    };
-  }, [isMobileMenuVisible]);
+  //         handleAppClassName();
+  //       },
+  //       true
+  //     );
+  //   }
+  //   return () => {
+  //     console.log('UNMOUNT');
+  //   };
+  // }, [isMobileMenuVisible]);
 
   /* const handleMenuDisplay = (e) => {
     console.log('menu display');
@@ -53,17 +57,18 @@ const NavbarBtn = ({ setMenuDisplay, hidden, visible, app }) => {
     setMenuDisplay((prev) => (prev === hidden ? visible : hidden));
   }; */
 
-  const handleMenuDisplay = (e) => {
-    e.stopPropagation();
-    e.nativeEvent.stopImmediatePropagation();
-    setIsMobileMenuVisible((prev) => !prev);
-    isMobileMenuVisible
-      ? docWrapper.current.classList.remove('navLinksMobileHidden')
-      : docWrapper.current.classList.add('navLinksMobileHidden');
+  // const handleMenuDisplay = (e) => {
+  //   e.stopPropagation();
+  //   e.nativeEvent.stopImmediatePropagation();
+  //   setIsMobileMenuVisible((prev) => !prev);
+  //   isMobileMenuVisible
+  //     ? docWrapper.current.classList.remove('navLinksMobileHidden')
+  //     : docWrapper.current.classList.add('navLinksMobileHidden');
+  // };
 
-    // setMenuDisplay((prev) => (prev === hidden ? visible : hidden));
+  const handleMenuDisplay = () => {
+    setMobileMenuVisible((prev) => (prev === 'hidden' ? 'visible' : 'hidden'));
   };
-
   return (
     <button
       onPointerDown={handleMenuDisplay}
