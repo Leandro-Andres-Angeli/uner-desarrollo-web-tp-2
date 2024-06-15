9f4d68d6216ebd38c533f5afea28f2495a72980a
Nahir-Orellana-Lopez <nahir.orellana.lopez@gmail.com>
2024-06-15 01:15:28 -0300

    Se implementó carrusel dinámico en Alojamiento | Se conectó imágenes con API

--- src/dbEndpoints.js
+++ src/dbEndpoints.js
@@ -2,11 +2,13 @@ const baseURL = 'http://localhost:3001/';
 
 const tiposDeAlojamiento = 'tiposAlojamiento/';
 const getAllTiposDeAlojamiento = 'getTiposAlojamiento';
+const getTipoDeAlojamiento = "getTipoAlojamiento";
 const createTipoAlojamiento = 'createTipoAlojamiento';
 const putTipoAlojamiento = 'putTipoAlojamiento';
 const deleteTipoAlojamiento = 'deleteTipoAlojamiento';
 export const crudTipoAlojamientosEndpoints = {
   readAll: `${baseURL}${tiposDeAlojamiento}${getAllTiposDeAlojamiento}`,
+  readOne: `${baseURL}${tiposDeAlojamiento}${getTipoDeAlojamiento}`,
   POST: `${baseURL}${tiposDeAlojamiento}${createTipoAlojamiento}`,
   PUT: `${baseURL}${tiposDeAlojamiento}${putTipoAlojamiento}`,
   DELETE: `${baseURL}${tiposDeAlojamiento}${deleteTipoAlojamiento}`,
