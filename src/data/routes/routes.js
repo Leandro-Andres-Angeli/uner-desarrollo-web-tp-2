cffd137f5a2c5b9bd3e7f3c374c6a867881382a0
= <=>
2024-06-17 17:54:14 -0300

    fx typo

--- src/data/routes/routes.js
+++ src/data/routes/routes.js
@@ -1,36 +1,36 @@
-import About from "../../pages/About";
+import About from '../../pages/About';
 
-import Contact from "../../pages/Contact";
-import Home from "../../pages/Home";
-import Admin from "../../pages/admin/Admin";
-import Buscador from "../../pages/buscador/buscador";
+import Contact from '../../pages/Contact';
+import Home from '../../pages/Home';
+import Admin from '../../pages/admin/Admin';
+import Buscador from '../../pages/buscador/Buscador';
 
 export const routes = [
   {
-    text: "home",
-    route: "/",
+    text: 'home',
+    route: '/',
     exact: true,
     component: <Home></Home>,
   },
   {
-    text: "buscar",
-    route: "/buscar",
+    text: 'buscar',
+    route: '/buscar',
     component: <Buscador></Buscador>,
   },
   {
-    text: "contacto",
-    route: "/contacto",
+    text: 'contacto',
+    route: '/contacto',
     component: <Contact></Contact>,
   },
   {
-    text: "acerca",
-    route: "/acerca",
+    text: 'acerca',
+    route: '/acerca',
     component: <About></About>,
   },
 
   {
-    text: "admin",
-    route: "/admin",
+    text: 'admin',
+    route: '/admin',
     exact: false,
     component: <Admin></Admin>,
   },
