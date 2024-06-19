47793408648ef20a0b38b53ca2d49333975450ef
Nahir-Orellana-Lopez <nahir.orellana.lopez@gmail.com>
2024-06-18 22:52:59 -0300

    Fix de HomeMain y Card

--- src/components/HomeMain.js
+++ src/components/HomeMain.js
@@ -1,81 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import { SiteIcon } from '../icons/SiteIcon';
-import Opinion from './opinion/Opinion';
-import { listaOpiniones } from './opinion/listaOpiniones';
-import Carrusel from './carrusel/Carrusel';
-import { useHistory } from 'react-router-dom';
-import PaginaBuscador from '../pages/buscador/PaginaBuscador';
-import handleCRUD from '../utils/handleCrud';
-import {
-  crudAlojamientosEndpoints,
-  crudImagenes,
-  crudTipoAlojamientosEndpoints,
-} from '../dbEndpoints';
+import React from "react";
+import { SiteIcon } from "../icons/SiteIcon";
+import Opinion from "./opinion/Opinion";
+import { listaOpiniones } from "./opinion/listaOpiniones";
+import Carrusel from "./carrusel/Carrusel";
+import { useHistory } from "react-router-dom";
+import PaginaBuscador from "../pages/buscador/PaginaBuscador";
 
 const HomeMain = () => {
   const history = useHistory();
-  const intialState = {
-    data: [],
-    loading: true,
-    done: false,
-    error: null,
-    update: false,
-  };
-  const [alojamientos, setAlojamientos] = useState(intialState);
-  const [imgs, setImgs] = useState(intialState);
-  const [tipoAlojamientos, setTipoAlojamientos] = useState(intialState);
-  const [joined, setJoinedData] = useState({ data: [] });
-
-  useEffect(() => {
-    return async () => {
-      // await handleCRUD(
-      //   crudAlojamientosEndpoints.readAll,
-      //   undefined,
-      //   setAlojamientos
-      // );
-      await Promise.all([
-        handleCRUD(
-          crudAlojamientosEndpoints.readAll,
-          undefined,
-          setAlojamientos
-        ),
-        handleCRUD(crudImagenes.readAll, undefined, setImgs),
-        handleCRUD(
-          crudTipoAlojamientosEndpoints.readAll,
-          undefined,
-          setTipoAlojamientos
-        ),
-      ])
-        .then(([alojamientos, pics, tipos]) => {
-          console.log(tipos);
-          setJoinedData((prev) => {
-            return {
-              ...prev,
-              data: alojamientos.map((el) => ({
-                ...el,
-                desc: tipos.filter(
-                  (aloj) => aloj.idTipoAlojamiento === el.TipoAlojamiento
-                )[0].Descripcion,
-                pictures: pics.filter(
-                  (pic) => pic.idAlojamiento === el.idAlojamiento
-                ),
-              })),
-            };
-          });
-        })
-        .catch((err) => console.log(err));
-    };
-  }, []);
-  const { data: dataAlojamientos } = alojamientos;
-  const { data: dataImgs } = imgs;
 
   return (
-    <main className='main main-index'>
-      <div className='chapa'></div>
-      <section className='contenido'>
-        {/* {JSON.stringify(dataImgs)}
-        {JSON.stringify(dataAlojamientos)} */}
-        {JSON.stringify(joined)}
+    <main className="main main-index">
+      <div className="chapa"></div>
+      <section className="contenido">
         <h1>Tu lugar en el mundo</h1>
         <p>
           Encontrá en un solo sitio el lugar perfecto para descansar y
@@ -84,23 +21,23 @@ const HomeMain = () => {
         <p>Podrás elegir el alojamiento que se adapte a tus necesidades.</p>
         <p>Estas a un click de distancia de ese lugar de ensueño</p>
         <button
-          onClick={() => history.push('/buscar')}
-          className='icon-btn btn-index'
+          onClick={() => history.push("/buscar")}
+          className="icon-btn btn-index"
         >
-          <SiteIcon className={'nav-logo-icon'}></SiteIcon>
+          <SiteIcon className={"nav-logo-icon"}></SiteIcon>
           Buscar Alojamiento
         </button>
       </section>
-      <section className='sobre-chapa'>
+      <section className="sobre-chapa">
         <PaginaBuscador
-          estado='Disponible'
+          estado="Disponible"
           mostrarFiltro={false}
           cantidad={4}
-          titulo='Recomendados'
+          titulo="Recomendados"
         ></PaginaBuscador>
       </section>
-      <section className='sobre-chapa'>
-        <h2>{'Lo que dicen nuestros clientes'}</h2>
+      <section className="sobre-chapa">
+        <h2>{"Lo que dicen nuestros clientes"}</h2>
 
         <Carrusel>
           {listaOpiniones.map((opinion, index) => {
--- src/pages/cards/Card.jsx
+++ src/pages/cards/Card.jsx
@@ -1,33 +1,27 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Cards.css';
-import Carrusel from '../../components/carrusel/Carrusel';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Cards.css";
+import Carrusel from "../../components/carrusel/Carrusel";
 
 const Card = ({ item }) => {
   return (
-    <div className='tarjeta-alojamiento'>
+    <div className="tarjeta-alojamiento">
       <div>
         {item.Imagenes.length > 0 ? (
           <Carrusel arrowSize={10}>
-            {item.Imagenes.map((imagen) => {
-              console.log(imagen);
-              return (
-                <img
-                  src={`./images/tipo_alojamientos_pics/${imagen.RutaArchivo}`}
-                  alt=''
-                />
-              );
+            {item.Imagenes.map((imagen, index) => {
+              return <img key={index} src={imagen.RutaArchivo} alt="" />;
             })}
           </Carrusel>
         ) : (
-          <img src='./images/tipo_alojamientos_pics/broken-image.png' alt='' />
+          <img src="images/img7.png" alt="" />
         )}
       </div>
       <Link to={`/alojamiento/${item.idAlojamiento}`}>
-        <div className='tarjeta-descripcion'>
+        <div className="tarjeta-descripcion">
           <h3>
             <strong>
-              {item.desc} en {item.Titulo}
+              {item.TipoAlojamiento} en {item.Titulo}
             </strong>
           </h3>
           {/* <p>
